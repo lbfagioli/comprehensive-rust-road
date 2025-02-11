2343a37f32b8c0452d3ff63ba1c72a612186881c
lbfagioli <lbfagioli@miuandes.cl>
2025-02-11 20:13:15 -0300

    finished binary tree 21.4

--- src/day3/morning.rs
+++ src/day3/morning.rs
@@ -128,6 +128,92 @@ impl Pet for Cat {
   }
 }
 
+// 21.4
+#[derive(Debug)]
+struct Node<T: Ord> {
+  value: T,
+  left: Subtree<T>,
+  right: Subtree<T>,
+}
+
+#[derive(Debug)]
+struct Subtree<T: Ord>(Option<Box<Node<T>>>);
+
+#[derive(Debug)]
+struct BinaryTree<T: Ord> {
+  root: Subtree<T>,
+}
+
+impl<T: Ord> Subtree<T> {
+  fn new() -> Self {
+    Self(None)
+  }
+
+  fn insert(&mut self, value: T) {
+    match &mut self.0 {
+      Some(node) => {
+        if value == node.value {
+          return;
+        }
+        if value > node.value {
+          node.right.insert(value);
+        } else {
+          node.left.insert(value);
+        }
+      }
+      None => {
+        self.0 = Some(Box::new(Node { value: value, left: Subtree::new(), right: Subtree::new() }));
+      }
+    }
+  }
+
+  fn has(&self, value: &T) -> bool {
+    match &self.0 {
+      Some(node) => {
+        if node.value == *value {
+          return true;
+        } else if *value > node.value {
+          return node.right.has(value);
+        } else {
+          return node.left.has(value);
+        }
+      }
+      None => return false,
+    }
+  }
+
+  fn len(&self) -> usize {
+    let mut l: usize = 0;
+    match &self.0 {
+      Some(node) => {
+        l += 1;
+        l += node.left.len();
+        l += node.right.len();
+      }
+      None => {}
+    }
+    return l;
+  }
+}
+
+impl<T: Ord> BinaryTree<T> {
+  fn new() -> Self {
+    Self { root: Subtree::new() }
+  }
+
+  fn insert(&mut self, value: T) {
+    self.root.insert(value);
+  }
+
+  fn has(&self, value: &T) -> bool {
+    self.root.has(value)
+  }
+
+  fn len(&self) -> usize {
+    self.root.len()
+  }
+}
+
 pub fn run() {
   // 20.4
   println!("");
@@ -208,4 +294,51 @@ pub fn run() {
   println!("dog size: {}, cat size: {}", std::mem::size_of::<Dog>(), std::mem::size_of::<Cat>());
   println!("&dog size: {}, &cat size: {}", std::mem::size_of::<&Dog>(), std::mem::size_of::<&Cat>());
   println!("&dyn size: {}, box<dyn> size: {}", std::mem::size_of::<&dyn Pet>(), std::mem::size_of::<Box<dyn Pet>>());
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn len() {
+        let mut tree = BinaryTree::new();
+        assert_eq!(tree.len(), 0);
+        tree.insert(2);
+        assert_eq!(tree.len(), 1);
+        tree.insert(1);
+        assert_eq!(tree.len(), 2);
+        tree.insert(2); // not a unique item
+        assert_eq!(tree.len(), 2);
+    }
+
+    #[test]
+    fn has() {
+        let mut tree = BinaryTree::new();
+        fn check_has(tree: &BinaryTree<i32>, exp: &[bool]) {
+            let got: Vec<bool> =
+                (0..exp.len()).map(|i| tree.has(&(i as i32))).collect();
+            assert_eq!(&got, exp);
+        }
+
+        check_has(&tree, &[false, false, false, false, false]);
+        tree.insert(0);
+        check_has(&tree, &[true, false, false, false, false]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(4);
+        check_has(&tree, &[true, false, false, false, true]);
+        tree.insert(3);
+        check_has(&tree, &[true, false, false, true, true]);
+    }
+
+    #[test]
+    fn unbalanced() {
+        let mut tree = BinaryTree::new();
+        for i in 0..100 {
+            tree.insert(i);
+        }
+        assert_eq!(tree.len(), 100);
+        assert!(tree.has(&50));
+    }
 }
