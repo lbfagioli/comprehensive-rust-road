794e36c1b032bbd45655c1ed5854ffefe464ac56
lbfagioli <lbfagioli@miuandes.cl>
2025-02-20 23:57:49 -0300

    28.4

--- src/day4/morning.rs
+++ src/day4/morning.rs
@@ -229,7 +229,89 @@ mod tests {
 }
 
 // 28.3
-#[deny(clippy::cast_possible_truncation)]
+// #[deny(clippy::cast_possible_truncation)]
+
+// 28.4
+pub fn luhn(cc_number: &str) -> bool {
+  let mut sum = 0;
+  let mut double = false;
+  let mut digit_count = 0;
+
+  for c in cc_number.chars().rev() {
+      if let Some(digit) = c.to_digit(10) {
+          if double {
+              let double_digit = digit * 2;
+              sum +=
+                  if double_digit > 9 { double_digit - 9 } else { double_digit };
+          } else {
+              sum += digit;
+          }
+          double = !double;
+          digit_count += 1;
+      } else if c != ' ' {
+          return false;
+      } else {
+        continue;
+      }
+  }
+
+  if digit_count < 2 {
+    return false;
+  }
+
+  sum % 10 == 0
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_valid_cc_number() {
+      assert!(luhn("4263 9826 4026 9299"));
+      assert!(luhn("4539 3195 0343 6467"));
+      assert!(luhn("7992 7398 713"));
+  }
+
+  #[test]
+  fn test_invalid_cc_number() {
+      assert!(!luhn("4223 9826 4026 9299"));
+      assert!(!luhn("4539 3195 0343 6476"));
+      assert!(!luhn("8273 1232 7352 0569"));
+  }
+
+  #[test]
+  fn test_first_mine() {
+    assert_eq!(luhn("43"), false);
+    assert_eq!(luhn(""), false);
+    assert_eq!(luhn("0    0"), true);
+  }
+
+  #[test]
+  fn test_non_digit_cc_number() {
+      assert!(!luhn("foo"));
+      assert!(!luhn("foo 0 0"));
+  }
+
+  #[test]
+  fn test_empty_cc_number() {
+      assert!(!luhn(""));
+      assert!(!luhn(" "));
+      assert!(!luhn("  "));
+      assert!(!luhn("    "));
+  }
+
+  #[test]
+  fn test_single_digit_cc_number() {
+      assert!(!luhn("0"));
+  }
+
+  #[test]
+  fn test_two_digit_cc_number() {
+      assert!(luhn(" 0 0 "));
+  }
+}
+
 pub fn run() {
   println!("so 'day4' starts..");
 
