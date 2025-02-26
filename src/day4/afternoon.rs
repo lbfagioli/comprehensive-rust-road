4fe1ffe13ca319237a7883b9b7fa0f61f60a5be8
lbfagioli <lbfagioli@miuandes.cl>
2025-02-26 13:10:11 -0300

    30.8

--- src/day4/afternoon.rs
+++ src/day4/afternoon.rs
@@ -96,6 +96,82 @@ fn read_username4(path: &str) -> Result<String> {
   Ok(username)
 }
 
+// 30.8
+/// An operation to perform on two subexpressions.
+#[derive(Debug)]
+enum Operation {
+  Add,
+  Sub,
+  Mul,
+  Div,
+}
+
+/// An expression, in tree form.
+#[derive(Debug)]
+enum Expression {
+  /// An operation on two subexpressions.
+  Op { op: Operation, left: Box<Expression>, right: Box<Expression> },
+
+  /// A literal value
+  Value(i64),
+}
+
+#[derive(PartialEq, Eq, Debug)]
+struct DivideByZeroError;
+
+// The original implementation of the expression evaluator. Update this to
+// return a `Result` and produce an error when dividing by 0.
+fn eval(e: Expression) -> Result<i64, DivideByZeroError> {
+  match e {
+    Expression::Op { op, left, right } => {
+      let Ok(left) = eval(*left) else { // should have done let x = eval()?;
+        return Err(DivideByZeroError);
+      };
+      let Ok(right) = eval(*right) else { // should have done let x = eval()?;
+        return Err(DivideByZeroError);
+      };
+      match op { // could wrap entire match in a Ok(match op {...}) instead of returning Ok(left op right) for every instance
+        Operation::Add => Ok(left + right), // wrong
+        Operation::Sub => Ok(left - right), // wrong
+        Operation::Mul => Ok(left * right), // wrong
+        Operation::Div => if right != 0 {
+          Ok(left / right) // wrong
+        } else {
+          return Err(DivideByZeroError);
+        },
+      }
+    }
+    Expression::Value(v) => Ok(v),
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::*;
+
+  #[test]
+  fn test_error() {
+    assert_eq!(
+      eval(Expression::Op {
+        op: Operation::Div,
+        left: Box::new(Expression::Value(99)),
+        right: Box::new(Expression::Value(0)),
+      }),
+      Err(DivideByZeroError)
+    );
+  }
+
+  #[test]
+  fn test_ok() {
+    let expr = Expression::Op {
+      op: Operation::Sub,
+      left: Box::new(Expression::Value(20)),
+      right: Box::new(Expression::Value(10)),
+    };
+    assert_eq!(eval(expr), Ok(10));
+  }
+}
+
 pub fn run() {
   println!("\nday4::afternoon::run");
 
